2078d0334fe8856bfa9db17116da7dbc7a3f2650
Cleber Feitosa <cleber.feitosa@ifmt.edu.br>
2026-09-22 14:44:47 -0400

    Excpetion personalizada

---
+++ ecommerce/src/main/java/com/tads/ecommerce/resource/exception/StanderError.java
@@ -0,0 +1,19 @@
+package com.tads.ecommerce.resource.exception;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.Instant;
+
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
+public class StanderError {
+    private Instant timestamp;
+    private Integer status;
+    private String error;
+    private String message;
+    private String path;
+}
--- ecommerce/src/main/java/com/tads/ecommerce/service/CategoryService.java
+++ ecommerce/src/main/java/com/tads/ecommerce/service/CategoryService.java
@@ -3,6 +3,7 @@ package com.tads.ecommerce.service;
 import com.tads.ecommerce.dto.CategoryDTO;
 import com.tads.ecommerce.entity.Category;
 import com.tads.ecommerce.repository.CategoryRepository;
+import com.tads.ecommerce.service.exception.ResourceNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
@@ -29,7 +30,7 @@ public class CategoryService {
     @Transactional(readOnly = true)
     public CategoryDTO findById(Long id) {
         Optional<Category> obj = repository.findById(id);
-        Category entity = obj.get();
+        Category entity = obj.orElseThrow(()->new ResourceNotFoundException("Entity not Found!"));
 
         return new CategoryDTO(entity);
     }
